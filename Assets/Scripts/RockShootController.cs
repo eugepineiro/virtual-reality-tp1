37a9e77778ac220f9a27b17f6aa8d77bd095be35
eugepineiro <epineiro@itba.edu.ar>
2022-08-21 21:20:22 -0300

    Add rock shooter

---
+++ Assets/Scripts/RockShootController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RockShootController : MonoBehaviour
+{   
+    Rigidbody rockRigidBody; 
+    bool shot = false; 
+
+    void Start()
+    {
+        rockRigidBody = GetComponent<Rigidbody>();    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float time = Mathf.Repeat(Time.time, 5.0F); 
+
+        if ( time < 2.0) { 
+            rockRigidBody.position = new Vector3(-14,1,-5); 
+            rockRigidBody.velocity = Vector3.zero; 
+            shot = false; 
+        } else { 
+            if ( !shot ) {
+                shot = true;
+                rockRigidBody.AddForce(Camera.main.transform.forward * 1000.0F); 
+            }
+        }
+    }
+}
